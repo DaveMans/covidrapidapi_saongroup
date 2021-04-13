b2080fb6929030ea8592709184160e28bbf1c5ed
David Mansilla <rdavidmansilla@gmail.com>
2021-04-12 18:52:36 -0600

    refactor: added log model into covid api service

--- src/SaonGroupTest.Client/Services/CovidRapidApiService.cs
+++ src/SaonGroupTest.Client/Services/CovidRapidApiService.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using SaonGroupTest.Client.Models;
 
 namespace SaonGroupTest.Client.Services
@@ -13,13 +14,17 @@ namespace SaonGroupTest.Client.Services
     {
         private readonly IHttpClientFactory _factory;
         private readonly IConfiguration _config;
+        private readonly ILogger _logger;
 
         public CovidRapiApiService(
             IHttpClientFactory factory,
-            IConfiguration config)
+            IConfiguration config,
+            ILogger<CovidRapiApiService> logger)
+
         {
             _factory = factory;
             _config = config;
+            _logger = logger;
         }
 
 
@@ -28,6 +33,8 @@ namespace SaonGroupTest.Client.Services
         {
             var client = GetClient();
             var path = $"regions";
+            _logger.LogInformation(client.BaseAddress + path);
+
             var response = await client.GetFromJsonAsync<RegionDtoModel>(path);
             var result = response.data.OrderBy(x => x.name).Select(x => x).ToList();
             var allRegionsItem = new CountryDtoModel
@@ -76,6 +83,7 @@ namespace SaonGroupTest.Client.Services
         {
             var client = GetClient();
             var path = iso == "ALL" ? "reports" : $"reports?iso={iso}";
+            _logger.LogInformation(client.BaseAddress + path);
             var areAllRegions = iso == "ALL";
             var response = await client.GetFromJsonAsync<ReportVm>(path);
             var result = FormatReportData(response.data, areAllRegions);
